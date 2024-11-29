ff675cd69a3b76fa39a14a5735fbb091fd56184f
aayush <aayusingh440@gmail.com>
2024-11-29 19:37:08 +0530

    cart count done

--- src/views/Components/Navbar.js
+++ src/views/Components/Navbar.js
@@ -7,8 +7,12 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
 import { CiShoppingCart } from "react-icons/ci";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useSelector } from 'react-redux';
 
 function NavBar() {
+
+  const cart = useSelector((state) => state.cartReducer.cart);
+  const cartItemCount = cart.length;
   return (
     <Navbar expand="lg" className="bg-body-tertiary sticky-top shadow" style={{ padding: '0.5rem 1rem' }}>
       <Container fluid>
@@ -25,7 +29,7 @@ function NavBar() {
             <Nav.Link as={Link} to="/products" style={{ fontSize: '1rem', marginLeft: '1rem' }}>
               Products
             </Nav.Link>
-           
+
 
             {/* Disabled link for Orders */}
             <Nav.Link as={Link} to="/orders" style={{ fontSize: '1rem', marginLeft: '1rem' }}>
@@ -35,11 +39,36 @@ function NavBar() {
 
           {/* Cart Icon with margin from right */}
 
-          <div className="d-flex align-items-center" style={{ marginLeft: 'auto' }}>
+          {/* <div className="d-flex align-items-center" style={{ marginLeft: 'auto' }}>
           <Nav.Link as={Link} to="/cart" style={{ fontSize: '1rem', marginLeft: '1rem' }}>
           <CiShoppingCart style={{ fontSize: '1.5rem', marginRight: '1rem' }} />
             </Nav.Link>
           
+          </div> */}
+
+          <div className="d-flex align-items-center" style={{ marginLeft: 'auto' }}>
+            {/* Cart Link */}
+            <Nav.Link as={Link} to="/cart" style={{ fontSize: '1rem', marginLeft: '1rem' }}>
+              <div className="position-relative">
+                {/* Cart Icon */}
+                <CiShoppingCart style={{ fontSize: '1.5rem', marginRight: '1rem' }} />
+
+                {/* Conditionally render the badge if there are items in the cart */}
+                {cartItemCount > 0 && (
+                  <span
+                    className="badge rounded-circle bg-black position-absolute"
+                    style={{
+                      top: '-5px',
+                      right: '-5px',
+                      padding: '5px 10px',
+                      fontSize: '12px',
+                    }}
+                  >
+                    {cartItemCount}
+                  </span>
+                )}
+              </div>
+            </Nav.Link>
           </div>
         </Navbar.Collapse>
       </Container>
